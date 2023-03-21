077b42264aa8ec8f93dc8dda4084bf0cb666afe4
Rodolfo <rodolfo__BN@hotmail.com>
2023-03-21 14:07:36 -0300

    adding new module, detekt and jacoco

--- app/src/main/java/com/example/github_actions/MainActivity.kt
+++ app/src/main/java/com/example/github_actions/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.github_actions
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent()
+        intent.setClassName(
+            this@MainActivity,
+            "com.example.github_actions.custom_module.CustomActivity"
+        )
+        startActivity(intent)
     }
 }
